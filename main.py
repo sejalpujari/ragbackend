da4dabb1c52ef29837a6fb3593ce66589328c196
sejalpujari <2111cs020501@mallareddyuniversity.ac.in>
2026-01-26 16:54:15 +0530

    orginsssh

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Explainable RAG API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://frontendrag.vercel.app"],  # update later
+    allow_origins=["*"],  # update later
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
